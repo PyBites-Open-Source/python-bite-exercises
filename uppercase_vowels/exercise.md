57f7012db8c00226b3e565aebe28a7854dbe8b1c
xettrisomeman <bsomeman326@gmail.com>
2024-10-06 07:41:02 +0545

    formatted using ruff

--- uppercase_vowels/exercise.md
+++ uppercase_vowels/exercise.md
@@ -8,9 +8,7 @@ GitHub user to credit: xettrisomeman
 # Uppercase Vowels
 
 ---
- The Python function `uppercase_vowels` takes in `string` or `filename` and returns `string` with vowels uppercased. The `string` can contain punctuation, uppercase consonants but in the final result vowels should be uppercased and consonants should be lowercased and punctuations should be removed while preserving spaces.
-
-> Extension of `file` should not be uppercased.
+ The Python function `uppercase_vowels` takes in `string` or `filename` and returns `string` with vowels uppercased. The `string` can contain punctuation, uppercase consonants but in the final result vowels should be uppercased and consonants should be lowercased and punctuations should be removed while preserving spaces. Extension of `file` should not be uppercased.
 
 Examples of `extensions`:
 ```
--- uppercase_vowels/script.py
+++ uppercase_vowels/script.py
@@ -1,19 +1,19 @@
+from string import punctuation
+
+
 def uppercase_vowels(text: str):
-    vowels = "aeiouAEIOU"
-    punct = ".?-*=!`';:_,~"
-    extensions = [".mp3", ".jpg", ".jpeg", ".pdf", ".txt", ".mp4", ".png", ".exe"]
-    cleaned_text = ""
-    extension = ""
-    for ex in extensions:
-        if text.endswith(ex):
-            extension = ex
-            text = text.replace(ex, "")
-            break
-    for t in text:
-        if t in vowels:
-            cleaned_text += t.upper()
-        elif t in punct:
-            cleaned_text += " "
+    VOWELS = "aeiou"
+    EXTENSIONS = [".mp3", ".jpg", ".jpeg", ".pdf", ".txt", ".mp4", ".png", ".exe"]
+    extension = next((ex for ex in EXTENSIONS if text.endswith(ex)), "")
+    text = text.removesuffix(extension)
+    transformed_chars = []
+    for char in text:
+        if char.lower() in VOWELS:
+            transformed_chars.append(char.upper())
+        elif char in punctuation:
+            transformed_chars.append(" ")
         else:
-            cleaned_text += t.lower()
+            transformed_chars.append(char.lower())
+
+    cleaned_text = "".join(transformed_chars)
     return cleaned_text + extension if extension else cleaned_text
